31e0a36563814edee1b5d50522daae56965967fb
constantinelarionoff <constantinelarionoff@gmail.com>
2020-10-28 00:11:12 +0200

    fixed stuff

--- main.c
+++ main.c
@@ -9,22 +9,23 @@ u8 main(void)
         // str is the input string;
         char str[LEN];
         puts("Enter the sentence:");
-        fgets(str, LEN, stdin);
+        fgets(str, LEN / 2, stdin);
         puts(" "); 
         // number of words in inputa;
         const u8 WORDS = words(str); 
 
         // the longest word in input;
-        const char L = longest(str);
+        const u8 L = longest(str);
+        
 
         // allocation for the array of pointers to words;
         char **w= (char **) malloc(sizeof(char **) * WORDS);
         for (u8 i = 0; i < WORDS; i++) {
-                w[i] =(char *) malloc(sizeof(char *) * L);
+                w[i] =(char *) malloc(sizeof(char *) * L + 4);
         }
         
         // tokenisation of str; words are stored in a;
-        char *a[WORDS];
+        char **a = (char **) malloc(WORDS * sizeof(char *) + 4);
         const char s[2] = " ";
         *a = strtok(str, s);
         u8 i = 1;
@@ -36,15 +37,15 @@ u8 main(void)
         // copying words to an array of words for like no reason;
         for (i = 0; i < WORDS; i++) 
                 strcpy(w[i], a[i]);
-        
+        free(a);
+
         puts("Printing one by one:");
         for (i = 0; i < WORDS; i++) 
                         puts(*(w + i));
 
         // a new char array; 
-        char sentence[WORDS * L];
-        sentence[0] = 32;
-
+        char *sentence = (char *) malloc(2* WORDS * L * sizeof(char));
+        sentence[0] = 0;
         // putting words back in the same sentence in order
         // to highlight the existantial nature of our existance;
         for (i = 0; i < WORDS; i++) {
@@ -54,8 +55,9 @@ u8 main(void)
 
         for (u8 i = 0; i < WORDS; i++) 
                 free(w[i]); 
-        free(w);    
+        free(w);
         puts("Printing together:");
-        puts(sentence + 1);
+        puts(sentence);
+        free(sentence); 
         return 0;
 }
--- words.h
+++ words.h
@@ -6,6 +6,7 @@
 typedef uint8_t u8;
 
 // returns lenght of the longest word in string;
+// overcounts by one if last word is longest but who cares;
 u8 longest(char *str)
 {
         u8 max(u8 a, u8 b)
@@ -19,19 +20,24 @@ u8 longest(char *str)
         u8 m = 0;
         u8 longest = 0;
         for (u8 i = 0; str[i]; i++) {
-                if (!(str[i] - 32)) 
+                if (str[i] - 32) {
                         m++;
-                else 
-                longest = max(longest, m);
+                } else {
+                        puts("m = 0");
+                        longest = max(longest, m);
+                        m = 0;
                 }
-        return longest;
+        }
+                
+        return max(longest, m);
 }
 
 // counts words in string;
+// overcounts if ends with a space; 
 u8 words(char *str)
 {
         u8 w = 1;
-        if (*str == 32)
+        if (*str == 32) 
                 w--;
         for (u8 i = 0; str[i]; i++) {
                 if (!(str[i] - 32) && str[i + 1] - 32) {
